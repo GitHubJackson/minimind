a7abebd1b89a7888c4b1314eb0ca1c76af004698
周伟滨 <lucas@zhouweibindeMacBook-Pro-2.local>
2024-01-21 00:11:52 +0800

    update public path

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,24 @@ import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), cssInjectedByJsPlugin()],
-  build: {
-    lib: {
-      entry: "./src/index.ts",
-      name: "minimind",
-      // formats: ['es'],
-      fileName: (format) => `minimind.${format}.js`,
-    },
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ["react"],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          react: "React",
-        },
-      },
-    },
-    outDir: "lib/dist",
-  },
+  base: "/minimind",
+  // build: {
+  //   lib: {
+  //     entry: "./src/index.ts",
+  //     name: "minimind",
+  //     // formats: ['es'],
+  //     fileName: (format) => `minimind.${format}.js`,
+  //   },
+  //   rollupOptions: {
+  //     // 确保外部化处理那些你不想打包进库的依赖
+  //     external: ["react"],
+  //     output: {
+  //       // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+  //       globals: {
+  //         react: "React",
+  //       },
+  //     },
+  //   },
+  //   outDir: "lib/dist",
+  // },
 });
